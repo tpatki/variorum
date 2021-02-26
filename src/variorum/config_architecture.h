48490c266f0c94fe8cc1585ec79072cb407e1c00
Tapasya Patki <patki1@llnl.gov>
2021-02-26 08:07:05 -0800

    Variorum API: change variorum_set_* to variorum_cap_*. Only a top-level API change has been made at this point. Internal functions that are not visible to users for Intel/IBM still say 'set' .

--- src/variorum/config_architecture.h
+++ src/variorum/config_architecture.h
@@ -100,7 +100,7 @@ struct platform
     /// @param [in] node_power_limit Desired node power limit in Watts.
     ///
     /// @return Error code.
-    int (*variorum_set_best_effort_node_power_limit)(int node_power_limit);
+    int (*variorum_cap_best_effort_node_power_limit)(int node_power_limit);
 
     /// @brief Function pointer to set a power limit to each node and then
     //  verify that the cap was set correctly.
@@ -108,7 +108,7 @@ struct platform
     /// @param [in] node_power_limit Desired node power limit in Watts.
     ///
     /// @return Error code.
-    int (*variorum_set_and_verify_node_power_limit)(int node_power_limit);
+    int (*variorum_cap_and_verify_node_power_limit)(int node_power_limit);
 
     /// @brief Set the GPU power shifting ratio (uniform across sockets).
     ///
@@ -119,14 +119,14 @@ struct platform
     //  Same ratio will be set on both sockets.
     ///
     /// @return Error code.
-    int (*variorum_set_gpu_power_ratio)(int gpu_power_ratio);
+    int (*variorum_cap_gpu_power_ratio)(int gpu_power_ratio);
 
     /// @brief Function pointer to set a power limit to each socket.
     ///
     /// @param [in] socket_power_limit Desired socket power limit in Watts.
     ///
     /// @return Error code.
-    int (*variorum_set_each_socket_power_limit)(int socket_power_limit);
+    int (*variorum_cap_each_socket_power_limit)(int socket_power_limit);
 
     int (*variorum_cap_each_core_frequency)(int core_freq_mhz);
 
